46222c6014553d3db48c69bbbf7bab701e76c622
npalgit <nxp121830@utdallas.edu>
2016-01-12 11:58:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ var tracking = require('./TrackApi.js');
 //Fedex: tracking.trackFedex(username, password, accountNumber, meterNumber, trackingNumber, callback);
 
 app.set('port', (process.env.PORT || 5000));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', function (req, res) 
 {
 
